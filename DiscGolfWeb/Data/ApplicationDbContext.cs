11d0cf61e9bc7695931bc6dbe8bb6d8209b26672
jfinley6 <johnfinleybremerton@gmail.com>
2023-11-09 14:53:11 -0700

    Added seed data to Categroy table

--- DiscGolfWeb/Data/ApplicationDbContext.cs
+++ DiscGolfWeb/Data/ApplicationDbContext.cs
@@ -7,10 +7,18 @@ namespace DiscGolfWeb.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Putting", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Driving", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Full Round", DisplayOrder = 3 }
+                ); ;
+        }
     }
 }
- 
