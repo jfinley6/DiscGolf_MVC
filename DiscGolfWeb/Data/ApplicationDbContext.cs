a905d1e2cf499f249a66657ebbb0b7adf554ae23
jfinley6 <johnfinleybremerton@gmail.com>
2023-11-08 19:30:51 -0700

    Create Connection String and Migrate Database

---
+++ DiscGolfWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DiscGolfWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscGolfWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+ 
